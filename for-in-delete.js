2c263b133f2d02de9bea306b28d6d704fd443cb4
colin smith <colincraigsmith@colins-MacBook-Pro.local>
2020-10-11 22:30:09 -0600

    for-in-delete submission

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,9 +40,12 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let string = ""
+  for(var prop in obj){
+    string += obj[prop];
+  }
+  return string
 }
-
 
 
 ////////// PROBLEM 2 //////////
@@ -55,6 +58,13 @@ function showValues( obj ) {
 
 //Code Here
 
+function greaterThan10(obj){
+  for(let prop in obj){
+    if(obj[prop] > 10){ 
+      obj[prop] = 0
+    }
+  }return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +76,11 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for(let prop in obj){
+    obj[prop] *= 2
+  } return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +94,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function secrets(obj){
+let string = ""
+for(let prop in obj){
+  if(prop.includes("sh")){
+    string += obj[prop];
+  }
+} return string
+}
 
 
 /* 
@@ -111,6 +132,12 @@ function showValues( obj ) {
 */
 
 //Code Here
+function removePassword(obj){
+  delete obj.password;{
+    return obj
+  }
+}
+
 
 
 
@@ -130,7 +157,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(let prop in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[prop] > 100){
+    delete deleteTheBigNumbers.third;
+    delete deleteTheBigNumbers.fourth;
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,7 +175,14 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function startsWithK(obj){
+  for(let prop in obj){
+    if(prop[0] === "k"){
+      delete obj[prop]
+    }
+  } return obj
+}
+ 
 
 
 ////////// PROBLEM 8 //////////
@@ -158,5 +197,10 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
-
+function hiddenTreasure(obj){
+  for(let prop in obj){
+    if(!obj[prop].includes("treasure")){
+      delete obj[prop]
+    }
+} return obj
+}
